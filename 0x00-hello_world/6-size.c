5b0521295c29aa1441daabea10a29a0b2474ad64
DAINAWAMBUI <daina.muiruri@gmail.com>
2022-12-08 13:34:39 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,27 +4,19 @@
  * code by diana
  * Return: 0
 */
+
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float e;
+int a;
+long int b;
+long long int c;
+char d;
+float e;
 
-	printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(d));
-	printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeo(a));
-	printf("Size of long int: %lu byte(S)\n", (unsigned long)sizeof(b));
-	printf("Size of long long int: %lu byte(S)\n", (unsigned long)sizeo(c));
-	printf("Size offloat: %lu byte(S)\n", (unsigned long)sizeof(e));
-	return (0);
-
-
-
-
-
-
-      
+printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(d));
+printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(e));
+return (0);
 }
- 
- 
