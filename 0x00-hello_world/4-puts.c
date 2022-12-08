660c6753a06e0c9dc62bbc74b296f0b7fc324df4
DAINAWAMBUI <daina.muiruri@gmail.com>
2022-12-08 10:09:40 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * main - Programming is like building a multilingual puzzle
+ * main - print Programming is like building a multilingual puzzle using puts
  *
  * Return: 0
 */
