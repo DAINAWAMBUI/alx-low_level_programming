d192a52c20f54898386e7982ebb377adf352154d
DAINAWAMBUI <daina.muiruri@gmail.com>
2022-12-10 20:44:01 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,7 +17,7 @@ int main(void)
 			for (ones = '0'; ones <= '9'; ones++) 
 			{
 				if (!((ones == tens) || (tens == hundreds) || 
-							(tens > ones) || (hundreds > tens))) /*eliminates repetition*/
+							(tens == ones) || (hundreds == tens))) /*eliminates repetition*/
 				{
 					putchar(tens);
 					putchar(ones);
@@ -34,5 +34,4 @@ int main(void)
 	} 
 	putchar('\n');
 	return (0);
-}
-	
+	}	
