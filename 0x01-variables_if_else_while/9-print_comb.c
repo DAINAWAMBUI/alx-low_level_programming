8cea4af562ba5e083a74d58dbcbd9b5296780348
DAINAWAMBUI <daina.muiruri@gmail.com>
2022-12-11 11:44:06 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (0)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-{
-	int a;
 
-	for (a = 0; a < 10; a++)	
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
 	{
-		putchar(a + '0');
-		if (a < 9)
+		putchar(d);
+		if (d != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	puchar('\n');
-	return (0); }	
+	putchar('\n');
+	return (0);
+}
