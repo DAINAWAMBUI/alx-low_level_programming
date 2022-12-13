6ac4db1eaaaeb1180bb890753b409a1554a34486
DAINAWAMBUI <daina.muiruri@gmail.com>
2022-12-13 08:30:50 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabets - entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+
+}
+
