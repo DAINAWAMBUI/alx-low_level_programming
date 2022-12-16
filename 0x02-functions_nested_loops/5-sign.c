f4763400b2ae403dc821cc18cb90dfb9cb667209
DAINAWAMBUI <daina.muiruri@gmail.com>
2022-12-15 18:09:52 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,6 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the number of the sign to be printed
+ * Description: number of sign to be printed
  * Return: 1 if number is greater than zero
  * 0 of number is zero
  * -1 if the number is less than zero
