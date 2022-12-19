82fd6a35326e548e7b850788df0865c321950238
DAINAWAMBUI <daina.muiruri@gmail.com>
2022-12-19 04:31:16 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,8 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - generates rndom valid passwords
+ * main - generates random valid passwords
  * Return: 0 (on success).
  *
  */
