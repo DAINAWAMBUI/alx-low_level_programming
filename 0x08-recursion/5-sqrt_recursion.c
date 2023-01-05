6f4b0304b4e0df8f8337a291085b063ac4a3bb74
DAINAWAMBUI <daina.muiruri@gmail.com>
2023-01-05 05:20:07 -0800

    update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,7 +12,7 @@ int _evaluate(int i, int n)
 	if (n == 0 || n == 1)
 		return (n);
 
-	else if (i * i == n)
+	else if (i * i < n)
 		return (_evaluate(i + 1, n));
 
 	else if (i * i == n) /*condition base*/
@@ -20,4 +20,21 @@ int _evaluate(int i, int n)
 
 	return (-1);
 	return (-i);
+}
+/**
+ * *_sqrt_recursion - evaluate sqrt
+ * *@n: integer
+ * *Return: sqrt_recursion
+ * */
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+	if (i < 0) /*if n is negative*/
+		return (-1);
+
+	else
+	{
+		return (_evaluate(i, n)); /*recursive call*/
+	}
 }
