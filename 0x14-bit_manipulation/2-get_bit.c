5be35e64d6b78794a15f24c42d5b6be7237877c0
DAINAWAMBUI <daina.muiruri@gmail.com>
2023-02-06 19:57:23 -0800

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,13 +6,12 @@
  * Return: -1 if an array
  * or the value of bit at index
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	
 	if ((n & (1 << index)) == 0)
-	return (-1);
-	
+		return (0);
 	return (1);
-}	
+}
